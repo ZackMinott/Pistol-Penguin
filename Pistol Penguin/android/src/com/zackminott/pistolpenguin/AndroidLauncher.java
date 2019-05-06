758a37f12ac5922f8a62c29c5221ae20ada174a2
ZackMinott <zacharyminott1997@gmail.com>
2019-05-06 11:27:10 -0700

    Added GDX

---
+++ Pistol Penguin/android/src/com/zackminott/pistolpenguin/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.zackminott.pistolpenguin;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.zackminott.pistolpenguin.PistolPenguin;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PistolPenguin(), config);
+	}
+}
