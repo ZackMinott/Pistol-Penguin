d21e1ddbee53c40f28af2d9f5c2bc8297c43df43
ZackMinott <zacharyminott1997@gmail.com>
2019-05-08 11:18:59 -0700

    Added in tubes and jumping

--- Pistol Penguin/core/src/com/zackminott/pistolpenguin/PistolPenguin.java
+++ Pistol Penguin/core/src/com/zackminott/pistolpenguin/PistolPenguin.java
@@ -10,11 +10,31 @@ public class PistolPenguin extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
 
+	Texture[] birds;
+	int flapState = 0;
+	float birdY = 0;
+	float velocity = 0;
+
+	int gameState = 0;
+	float gravity = 1.5f;
+
+	Texture topTube;
+	Texture bottomTube;
+	float gap = 400;
+
 	//Runs when the app is starts
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		background = new Texture("bg.png");
+
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
+		birdY = Gdx.graphics.getHeight()/2 - birds[0].getHeight()/2;
+
+		topTube = new Texture("toptube.png");
+		bottomTube = new Texture("bottomtube.png");
 	}
 
 	//Updates every frame
@@ -22,6 +42,36 @@ public class PistolPenguin extends ApplicationAdapter {
 	public void render () {
 		batch.begin(); //tells render method that we can start displaing sprites now
 		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()); //Draws the background to fit the width and height of the screen
+
+		if(gameState != 0){
+			batch.draw(topTube, Gdx.graphics.getWidth()/2 - topTube.getWidth()/2, Gdx.graphics.getHeight() / 2 + gap / 2);
+			batch.draw(bottomTube, Gdx.graphics.getWidth()/2 - bottomTube.getWidth()/2, Gdx.graphics.getHeight()/2 - gap/2 - bottomTube.getHeight());
+
+			if(Gdx.input.justTouched()){
+				velocity = -27;
+			}
+			//stops the bird at the bottom of the screen
+			if(birdY > 0 || velocity < 0){
+				velocity += gravity;
+				birdY -= velocity;
+			}
+
+		} else {
+			if(Gdx.input.justTouched()){
+				gameState = 1;
+			}
+		}
+
+		if(flapState == 0)
+			flapState = 1;
+		else
+			flapState = 0;
+
+
+		batch.draw(birds[flapState], Gdx.graphics.getWidth()/2 - birds[flapState].getWidth()/2, birdY);
+		batch.end();
+
+
 
 	}
 	
